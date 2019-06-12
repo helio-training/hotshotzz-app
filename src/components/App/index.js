7261d000da00b6130e119b7b3cdb441365f0b3a0
Drew <verwiel.code@gmail.com>
2019-06-12 13:44:48 -0600

    added login/signup

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import HomePage from '../../pages/HomePage'
-import RegisterPage from '../../pages/RegisterPage'
+import Login from '../Forms/Login'
 import UserPage from '../../pages/UserPage'
 
 export default () => (
   <>
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/register" component={RegisterPage}/>
+      <Route exact path="/register" component={Login}/>
       <Route exact path="/user" component={UserPage}/>
     </Switch>
   </>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router'
+import { AUTH_TOKEN } from '../constants'
 
-export default () => (
-  <div>
-    <h1>Homepage</h1>
-    <a href='/user'>User Page</a>
-    <br />
-    <a href='/register'>Register Page</a>
-  </div>
+const authToken = localStorage.getItem(AUTH_TOKEN)
+
+export default (props) => (
+  <>
+    <div>
+      <h1>Homepage</h1>
+      <Link to='/user'>User Page</Link>
+      <br />
+    </div>
+    <div>
+      {authToken ? (
+        <div
+          onClick={() => {
+            localStorage.removeItem(AUTH_TOKEN)
+            props.history.push(`/register`)
+          }}>
+          <button>Logout</button>
+        </div>
+      ) : (
+        <Link to="/register">
+          <button>Login</button>
+        </Link>
+      )}
+    </div>
+  </>
 )
